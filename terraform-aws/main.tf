43c59f7888d39d6db1e9f49b47f5131ba07c01e5
Botanhassan <beaughost262@gmail.com>
2025-07-16 23:28:36 +0100

    🔁 Relance du pipeline

--- terraform-aws/main.tf
+++ terraform-aws/main.tf
@@ -49,9 +49,5 @@ resource "aws_instance" "nginx_server" {
   provisioner "local-exec" {
     command = "echo ${self.public_ip} > ../ansible/inventory.ini"
   }
-  output "instance_public_ip" {
-  value = aws_instance.web.public_ip
-  }
-
 }
 # Ceci est un test pour relancer le pipeline maintenant
